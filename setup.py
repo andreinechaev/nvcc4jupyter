fef45947b376e2a162bdc0633629ac74582856be
Timoteo Colnaghi <19778186+project-tuva@users.noreply.github.com>
2019-12-02 15:13:01 +0100

    fixed typo in this repo's web address

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Andrei Nechaev',
     author_email='lyfaradey@yahoo.com',
     py_modules=['nvcc_plugin', 'v2.v2', 'v1.v1', 'common.helper'],
-    url='htpps://github.com/andreinechaev/nvcc4jupyter',
+    url='https://github.com/andreinechaev/nvcc4jupyter',
     license='LICENSE',
     description='Jupyter notebook plugin to run CUDA C/C++ code',
     # long_description=open('README.md').read(),
