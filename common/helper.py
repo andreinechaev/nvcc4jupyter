0b32d7128a42c96206247232477d1507fd0e164c
Ștefan-Cosmin Ciocan <57830279+cosminc98@users.noreply.github.com>
2023-12-18 23:05:36 +0100

    Add profile and profiler-args options to argument parser.

--- common/helper.py
+++ common/helper.py
@@ -3,8 +3,25 @@ import argparse
 
 def get_argparser():
     parser = argparse.ArgumentParser(description='NVCCPlugin params')
-    parser.add_argument("-t", "--timeit", action='store_true',
-                        help='flag to return timeit result instead of stdout')
+    parser.add_argument(
+        '-t', 
+        '--timeit',
+        action='store_true',
+        help='If set, returns the output of the "timeit" built-in ipython magic instead of stdout.',
+    )
+    parser.add_argument(
+        '-p', 
+        '--profile', 
+        action='store_true',
+        help='If set, runs the nvidia nsight compute profiler. Has no effect if used with --timeit.',
+    )
+    parser.add_argument(
+        '-a'
+        '--profiler-args',
+        type=str,
+        default='',
+        help='Extra options that can be passed to the nvidia nsight compute profiler.',
+    )
     return parser
 
 
