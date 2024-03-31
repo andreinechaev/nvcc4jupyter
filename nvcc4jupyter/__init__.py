1a8f3ff6eac9b7e78d8184901fd62e21eed68b49
Cosmin Ștefan Ciocan <57830279+cosminc98@users.noreply.github.com>
2024-03-31 09:37:16 +0000

    Bump version to 1.2.1

--- nvcc4jupyter/__init__.py
+++ nvcc4jupyter/__init__.py
@@ -5,4 +5,4 @@ nvcc4jupyter: CUDA C++ plugin for Jupyter Notebook
 from .parsers import Profiler, set_defaults  # noqa: F401
 from .plugin import NVCCPlugin, load_ipython_extension  # noqa: F401
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
